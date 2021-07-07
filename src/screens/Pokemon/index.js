5f350c69822f0699f13d817c08b344281f8170ad
Carlos Eduardo de Sousa Fiuza <carlosfiuza1234@gmail.com>
2021-07-07 07:41:58 -0300

    #5 Modificando Swiper de fotos e adicionando informaçoes Sobre e Status Base

--- src/screens/Pokemon/index.js
+++ src/screens/Pokemon/index.js
@@ -14,6 +14,10 @@ import Swiper from 'react-native-swiper';
 import { SvgUri } from 'react-native-svg';
 
 import PokeballBackground from '../../assets/pokeball.svg';
+import Ruler from '../../assets/ruler.svg';
+import WeightBalance from '../../assets/weightBalance.svg';
+import Previous from '../../assets/previous.svg';
+import Next from '../../assets/next.svg';
 
 import { Container,
          HeaderArea,
@@ -24,7 +28,40 @@ import { Container,
          LoadingIcon,
          SwipeDot,
          SwipeItem,
-         DetailsArea } from './styles';
+         DetailsArea,
+         PokeballBackgroundArea,
+
+         TypesArea,
+         TypeBox,
+         TypeName,
+         
+         AboutTitle,
+         AboutArea,
+         DivisorLine,
+
+         WeightArea,
+         WeightRow,
+         WeightNumber,
+         WeightName,
+
+         HeightArea,
+         HeightRow,
+         HeightNumber,
+         HeightName,
+
+         AbilitiesArea,
+         AbilityName,
+         AbilityTitle,
+
+         StatusArea,
+         StatusTypesArea,
+         StatusTypeName,
+         StatusTitle,
+         StatusNumbersArea,
+         StatusNumber,
+         StatusBar
+          } from './styles';
+
 
 export default () => {
 
@@ -59,21 +96,34 @@ export default () => {
                 }
             }
         }
+
+        let types = [];
+        for(let element in data['types']){
+            types.push(data['types'][element].type.name);
+        }
+
+        let abilities = [];
+        for(let element in data['abilities']){
+            abilities.push(data['abilities'][element].ability.name);
+        }
+
         setInfo({
             name: info.name,
             id: info.id,
             color: info.color,
             images: images,
-            types: data['types'],
-            abilities: data['abilities'],
+            types: types,
+            abilities: abilities,
             height: data['height'],
             weight: data['weight'],
-            hp: data['stats'][0].base_stats,
-            atk: data['stats'][1].base_stats,
-            def: data['stats'][2].base_stats,
-            satk: data['stats'][3].base_stats,
-            sdef: data['stats'][4].base_stats,
-            spd: data['stats'][5].base_stats
+            statusBase: [
+                'HP:' + data['stats'][0].base_stat,
+                'ATK:' + data['stats'][1].base_stat,
+                'DEF:' + data['stats'][2].base_stat,
+                'SATK:' + data['stats'][3].base_stat,
+                'SDEF:' + data['stats'][4].base_stat,
+                'SPD:' + data['stats'][5].base_stat
+            ]
         });
     };
 
@@ -87,23 +137,24 @@ export default () => {
                     <TitleArea>
                         <ArrorBackButton onPress={navigateBack}>
                             <Icon name="arrow-left" size={32} color="#FFFFFF"/>
-                            </ArrorBackButton>
+                        </ArrorBackButton>
                         <Title> {info.name} </Title>
                     </TitleArea>
                     <Id> {'#' + info.id} </Id>
-                </HeaderArea>
+            </HeaderArea>
+
+            <PokeballBackground width="100%" height="208" position='absolute' alignSelf='flex-end'/>
 
             {info.images && Object.keys(info.images).length > 0 ?
                 <Swiper
-                    style={{height: windowHeight * 0.33}}
-                    dot={<SwipeDot color='#FFFFFF'/>}
-                    activeDot={<SwipeDot color='#000000'/>}
-                    paginationStyle={{top: 15, right: 15, bottom: null, left: null}}
+                    style={{height: windowHeight * 0.26}}
                     autoplay={true}
+                    showsPagination={false}
+                    showsButton={true}
                     >
 
                     {info.images.map((element, key) => (
-                            <SwipeItem key={key} color={info.color}>
+                            <SwipeItem key={key}>
                                 {element.slice(-3) != 'svg' ?
                                     <Image source={{uri: `${element}`}}
                                         style={{
@@ -117,7 +168,6 @@ export default () => {
                                         height= {windowHeight * 0.25}
                                         resizeMode ="contain"
                                         uri= {element}
-                                        position = 'absolute'
                                     />
 
                                     }
@@ -126,12 +176,85 @@ export default () => {
                 </Swiper>
                 : 
                 <LoadingIcon size="large" color="#000000"/>
-                }
-                
-            <PokeballBackground width="100%" height="208" position='absolute' alignSelf="flex-end"/>
+            }
+            <DetailsArea color={info.color} marginTop={windowHeight * -0.7}>
 
-            <DetailsArea color={info.color} marginTop={windowHeight * -0.5}>
-                <LoadingIcon size="large" color="#000000"/>
+                {info.abilities && info.abilities.length > 0 &&
+                    <TypesArea marginTop={(windowHeight * 0.15)}>
+                        {info.types.map((element, key) => (
+                            <TypeBox key={key} color={info.color}>
+                                <TypeName key={key}> {element} </TypeName>
+                            </TypeBox>
+                        ))}
+                    </TypesArea>
+                }
+
+                <AboutTitle color={info.color}> Sobre </AboutTitle>
+
+                <AboutArea>
+                    {info.weight && 
+                        <WeightArea>
+                                <WeightRow>
+                                    <WeightBalance height="24" width="24" />
+                                    <WeightNumber> {info.weight.toString() +  ' kg'} </WeightNumber>
+                                </WeightRow>
+                            <WeightName> Peso </WeightName>
+                        </WeightArea>
+                    }
+
+                    <DivisorLine height={70}>
+                    </DivisorLine>
+
+                    {info.height &&
+                        <HeightArea>
+                                <HeightRow>
+                                    <Ruler height="24" width="24" />
+                                    <HeightNumber> {info.height.toString() + ' m'} </HeightNumber>
+                                </HeightRow>
+                            <HeightName> Altura </HeightName>
+                        </HeightArea>
+                    }
+
+                    <DivisorLine height={70}>
+                    </DivisorLine>
+
+                    {info.abilities && info.abilities.length > 0 &&
+                        <AbilitiesArea>
+                            {info.abilities.map((element, key) => (
+                                <AbilityName key={key}> {element} </AbilityName>
+                            ))}
+                            <AbilityTitle> Habilidades </AbilityTitle>
+                        </AbilitiesArea>
+                    }
+
+                </AboutArea>
+                {info.statusBase && <StatusTitle color={info.color}> Status Base </StatusTitle>}
+                {info.statusBase && info.statusBase.length > 0 && 
+                    <StatusArea>
+                        <StatusTypesArea>
+                            {info.statusBase.map((element, key) => (
+                                <StatusTypeName key={key} color={info.color}> {element.split(':')[0]} </StatusTypeName>
+                            ))}
+                        </StatusTypesArea>
+
+                        <DivisorLine height={140}>
+                        </DivisorLine>
+
+                        <StatusNumbersArea>
+                            {info.statusBase.map((element, key) => (
+                                <StatusNumber key={key}> {element.split(':')[1]} </StatusNumber>
+                            ))}
+                        </StatusNumbersArea>
+                        <StatusNumbersArea>
+                            {info.statusBase.map((element, key) => (
+                                <StatusBar key={key} color={info.color} width={parseInt(element.split(':')[1]) * (windowWidth *0.004)} />
+                            ))}
+
+                        </StatusNumbersArea>
+                    </StatusArea>
+                }
+        
+            
             </DetailsArea>
   
         </Container>
--- src/screens/Pokemon/styles.js
+++ src/screens/Pokemon/styles.js
@@ -23,6 +23,7 @@ export const ArrorBackButton= styled.TouchableOpacity`
     width: 40px;
     height: 40px;
     justify-content: center;
+    z-index: 9;
 `;
 
 export const Title = styled.Text`
@@ -55,16 +56,166 @@ export const SwipeDot = styled.View`
 
 export const SwipeItem = styled.View`
     flex: 1;
-    background-color: ${props => props.color};
-    z-index: 10;
+    z-index: 12;
 `;
 
 export const DetailsArea = styled.View`
     margin-top: ${props => props.marginTop + 'px'};
     border-radius: 30px;
-    border: 20px solid ${props => props.color};
+    border: 10px solid ${props => props.color};
     background-color: #FFFFFF;
     flex: 1;
     z-index: -1;
     flex-direction: column;
+`;
+
+export const TypesArea = styled.View`
+    margin-top: ${props => props.marginTop + 'px'};
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const TypeBox = styled.View`
+    background-color: ${props => props.color};
+    border-radius: 10px;
+    align-items: center;
+    padding: 5px;
+    margin: 5px;
+`;
+
+export const TypeName = styled.Text`
+    color: #FFFFFF;
+    font-size: 16px;
+    font-weight: bold;
+`;
+
+export const AboutTitle = styled.Text`
+    color: ${props => props.color};
+    font-size: 26px;
+    font-weight: bold;
+    alignSelf: center;
+
+`;
+
+export const AboutArea = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 10px;
+`;
+
+export const DivisorLine = styled.View`
+    background-color: #E0E0E0;
+    height: ${props => props.height + 'px'};
+    width: 2px;
+    margin: 3px;
+`;
+
+export const WeightArea = styled.View`
+    flex-direction: column;
+`;
+
+export const WeightRow = styled.View`
+    flex-direction: row;    
+`;
+
+export const WeightNumber = styled.Text`
+    color: #212121;
+    font-size: 16px;
+    font-weight: 400;
+    alignSelf: center;
+`;
+
+export const WeightName = styled.Text`
+    color: #666666;
+    font-size: 14px;
+    font-weight: 400;
+    alignSelf: center;
+    margin-top: 10px;
+`;
+
+export const HeightArea = styled.View`
+    flex-direction: column;
+`;
+
+export const HeightRow = styled.View`
+    flex-direction: row;
+`;
+
+export const HeightNumber = styled.Text`
+    color: #212121;
+    font-size: 16px;
+    font-weight: 400;
+    alignSelf: center;
+`;
+
+export const HeightName = styled.Text`
+    color: #666666;
+    font-size: 14px;
+    font-weight: 400;
+    alignSelf: center;
+    margin-top: 10px;
+`;
+
+export const AbilitiesArea = styled.View`
+    flex-direction: column;
+`;
+
+export const AbilityName = styled.Text`
+    color: #666666;
+    font-size: 16px;
+    font-weight: 400;
+`;
+
+export const AbilityTitle = styled.Text`
+    color: #666666;
+    font-size: 14px;
+    font-weight: 400;
+    alignSelf: center;
+    margin-top: 10px;
+`;
+
+export const StatusTitle = styled.Text`
+    color: ${props => props.color};
+    font-size: 24px;
+    font-weight: bold;
+    alignSelf: center;
+    margin-bottom: 30px;
+`;
+
+export const StatusArea = styled.View`
+    flex-direction: row;
+    justify-content: space-around;
+`;
+
+export const StatusTypesArea = styled.View`
+    flex-direction: column;
+    align-items: flex-end;
+`;
+
+export const StatusTypeName = styled.Text`
+    color: ${props => props.color};
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 3px;
+`;
+
+export const StatusNumbersArea = styled.View`
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+export const StatusNumber = styled.Text`
+    color: #000000;
+    font-size: 16px;
+    margin-bottom: 3px;
+`;
+
+export const StatusBar = styled.View`
+    background-color: ${props => props.color};
+    width: ${props => props.width + 'px'};;
+    height: 15px;
+    margin: 5px;
+    alignSelf: flex-start;
+    border-radius: 5px;
 `;
