c9165da5fe42dd22c48669f5e5b4b7c1519866e2
Carlos Eduardo de Sousa Fiuza <carlosfiuza1234@gmail.com>
2021-07-08 06:24:47 -0300

    #3 Realizando requisiçao com api antes de ir para tela Home

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -2,14 +2,42 @@ import React , {useEffect} from 'react';
 import { Container, Logo, Title, LoadingIcon} from './styles';
 import { useNavigation } from '@react-navigation/native';
 
+import Api from '../../Api';
+
 export default () => {
 
     const navigation = useNavigation();
 
-    useEffect(() => {
-        return (
-            setTimeout(() => { navigation.replace('Home') }, 3000)
-        );
+    const factoryObjects = (name, url, id, color, image) => {
+            return { name, url, id, color, image};
+    };
+
+    const upperCaseLetter = (string) => {
+        return string.slice(0,1).toUpperCase() + string.substring(1);
+    };
+    
+    useEffect(async() => {
+        let data = await Api.getPokedex();
+        let list = [{}];
+        data['pokemon_species'].map((element, key) => {
+            let id = element.url.substring(42);
+            id = id.slice(0, -1);
+            id = Number(id);
+            list.push(factoryObjects(
+                upperCaseLetter(element.name),
+                element.url,
+                id,
+                null,
+                null
+            )); 
+        });
+        list.shift();
+        list.sort((a, b) => {
+                return a.id > b.id;
+        });
+
+        setTimeout(() => { navigation.replace('Home', {data: list})} , 3500)
+
     }, []);
 
     return (
